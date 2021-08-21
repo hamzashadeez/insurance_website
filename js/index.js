7448a18320d325ac0553b447ed21304f02f6141b
hamzashadeez <hahmad11@gmail.com>
2021-08-21 07:42:52 -0700

    first commit

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const fullname = document.getElementById("fullname");
+const email = document.getElementById("email");
+const message = document.getElementById("message");
+
+document
+  .getElementById("message_form")
+  .addEventListener("submit", function (event) {
+    event.preventDefault();
+    let messageData = { fullname: fullname.value, email: email.value, message: message.value };
+    console.log(messageData)
+
+    fullname.value = "";
+    email.value = "";
+    message.value = "";
+    // alert("hamza    ")
+  });
